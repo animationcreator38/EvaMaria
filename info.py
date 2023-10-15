c19b15e30d5bb356e8173cec1ef619baa3314402
animationcreator38 <147336591+animationcreator38@users.noreply.github.com>
2023-10-15 15:26:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,9 +12,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ('API_ID', '28172775'))
-API_HASH = environ['ad113b766dae21232c58017ac217576b']
-BOT_TOKEN = environ['6624704482:AAFvMZbokVy8CSKa-mGnFLeI-TlyfXPeGVA']
+API_ID = int(environ.get('API_ID', '28172775'))
+API_HASH = environ.get('API_HASH', 'ad113b766dae21232c58017ac217576b')
+BOT_TOKEN = environ.get('BOT_TOKEN', "6624704482:AAFvMZbokVy8CSKa-mGnFLeI-TlyfXPeGVA")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -26,7 +26,7 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001879645869').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('-1001645894229')
+auth_channel = environ.get('AUTH_CHANNEL', '-1001645894229')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
